0f95afcbc8cc6e0e256f3cbb25d48726407d75de
caio00702 <caio.emnl.silva@gmail.com>
2021-01-23 15:23:02 -0300

    alula HTML5-CSS3

---
+++ js/main-js.js
@@ -0,0 +1,140 @@
+
+function clicou() {
+    document.getElementById("Clique aqui para mais").innerHTML = "<b>Clique aqui para mais</b>";
+    //console.log(document.getElementById("Clique aqui para mais"));
+    //alert("Clique aqui para mais")
+}
+
+function redirecionar() {
+    window.open("https://web.digitalinnovation.one/home");
+    //window.location.href = "https://digitalinnovation.one/";
+}
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+
+}
+
+function load() {
+    alert("pagina carregada")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+var validar =0;
+function validaIdade(idade) {
+    
+    if (idade >= 18) {
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual a sua idade");
+validaIdade(idade)
+console.log(validar);
+
+*/
+//console.log(validaIdade(idade));
+/*
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+*/
+//alert(soma(5, 10));
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+/*
+var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+    
+}
+*/
+/*
+var count=0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+/*
+//var idade = 18;
+var idade = prompt("Qual é a sua idade");
+if (idade >= 18) {
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+
+//lista.pop();
+
+//lista.push("uva");
+
+//console.log(lista.toString());
+
+//console.log(lista.reverse());
+
+//console.log(lista.length);
+
+//console.log(lista);
+//console.log(lista[0]);
+
+//console.log(lista.toString()[0]);
+
+//console.log(lista.join(" - "));
+//console.log(lista.join(" | "));
+//console.log(lista.join(" "));
+//console.log(lista.join(""));
+
+/*
+var nome = "Caio Silva";
+var n1 = 5;
+var n2 = 3;
+varfrase= ""Japão", "Brasil" é o melhor time do mundo";
+//alert(" Bem-vindo! " + nome + n1 + " anos");
+//alert(n1 + n2);
+console.log(nome);
+console.log(n1 + n2)
+console.log(frase.toLowerCase());
+//alert(frasereplae(""Japão", "Brasil"", "Brasil"));
+*/
